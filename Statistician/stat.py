6bd1e32cb3eccff459f36d9e9bded4963d089167
eee555 <2234208506@qq.com>
2022-04-01 23:37:26 +0800

    111

--- Statistician/stat.py
+++ Statistician/stat.py
@@ -1,18 +1,18 @@
 import ms_toollib as ms
-import os, click
+import os, click, sys
 import json
-from github3 import GitHub
+#from github3 import GitHub
+from io import StringIO
 
 # i.e.
-# python stat.py --video_dir ../videos --username ${{ github.repository_owner }} --token ${{ secrets.GITHUB_TOKEN }}
+# python stat.py --video_dir ../videos > v.json
 
 
 
 @click.command()
 @click.option('--video_dir', default='videos', help='录像的目录')
-@click.option('--username', default='videos', help='令牌')
-@click.option('--token', default='videos', help='令牌')
-def stat(video_dir, username, token):
+def stat(video_dir):
+    #click.echo('Hello World!')
     avfs = os.listdir(video_dir)
     json_list = []
     for avf in avfs:
@@ -24,10 +24,15 @@ def stat(video_dir, username, token):
     filename = "v.json"
 
 
-    gh = GitHub(token=token)
-    rep = gh.repository(username, '')
-    j = rep.file_contents('/v.json')
-    j.update('auto commit', json.dumps(json_list))
+    #gh = GitHub(token=token)
+    #rep = gh.repository(username, '')
+    #j = rep.file_contents('/v.json')
+    #j.update('auto commit', json.dumps(json_list))
+    #strIO = StringIO()
+    #sys.stdout = strIO
+    click.echo(json.dumps(json_list))
+    #print(strIO.tell())
+    # strIO.write(json.dumps(json_list))
     # with open(filename, 'w') as file_obj:
         # json.dump(json_list, file_obj)
 if __name__ == '__main__':
