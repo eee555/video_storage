4da0597585ede0ca25375397653634f7d2e8066a
eee555 <2234208506@qq.com>
2022-04-01 20:39:43 +0800

    Initial commit

---
+++ Statistician/stat.py
@@ -0,0 +1,25 @@
+import ms_toollib as ms
+import os, click
+import json
+
+# i.e.
+# python stat.py --video_dir ../videos
+
+@click.command()
+@click.option('--video_dir', default='videos', help='录像的目录')
+def stat(video_dir):
+    avfs = os.listdir(video_dir)
+    json_list = []
+    for avf in avfs:
+        avf = video_dir + r"/" + avf
+        a = ms.AvfVideo(avf)
+        a.parse_video()
+        a.analyse()
+        json_list.append({"player": a.player, "rtime": a.r_time, "bbbv": a.bbbv})
+    
+    filename = "v.json"
+    with open(filename, 'w') as file_obj:
+        json.dump(json_list, file_obj)
+if __name__ == '__main__':
+    stat()
+
