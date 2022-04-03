e6bff045f1a5ce15dd1e4e1b46a4372770a35755
eee555 <2234208506@qq.com>
2022-04-03 20:19:13 +0800

    fix: eee

--- Statistician/get_leaderboard.py
+++ Statistician/get_leaderboard.py
@@ -16,19 +16,19 @@ def stat(json_dir):
     for video in video_json:
         if video['player'] in leaderboard:
             if video['row'] == 8 and video['column'] == 8 and video['mine_num'] == 10:
-                if video['rtime'] < leaderboard[video['player']]['beg_time']:
+                if video['rtime'] < leaderboard[video['player']]['beg_time'][0]:
                     leaderboard[video['player']]['beg_time'] = [video['rtime'], video['file']]
-                if video['bbbv_s'] > leaderboard[video['player']]['beg_bbbv_s']:
+                if video['bbbv_s'] > leaderboard[video['player']]['beg_bbbv_s'][0]:
                     leaderboard[video['player']]['beg_bbbv_s'] = [video['bbbv_s'], video['file']]
             elif video['row'] == 16 and video['column'] == 16 and video['mine_num'] == 40:
-                if video['rtime'] < leaderboard[video['player']]['int_time']:
+                if video['rtime'] < leaderboard[video['player']]['int_time'][0]:
                     leaderboard[video['player']]['int_time'] = [video['rtime'], video['file']]
-                if video['bbbv_s'] > leaderboard[video['player']]['int_bbbv_s']:
+                if video['bbbv_s'] > leaderboard[video['player']]['int_bbbv_s'][0]:
                     leaderboard[video['player']]['int_bbbv_s'] = [video['bbbv_s'], video['file']]
             elif video['row'] == 16 and video['column'] == 30 and video['mine_num'] == 99:
-                if video['rtime'] < leaderboard[video['player']]['exp_time']:
+                if video['rtime'] < leaderboard[video['player']]['exp_time'][0]:
                     leaderboard[video['player']]['exp_time'] = [video['rtime'], video['file']]
-                if video['bbbv_s'] > leaderboard[video['player']]['exp_bbbv_s']:
+                if video['bbbv_s'] > leaderboard[video['player']]['exp_bbbv_s'][0]:
                     leaderboard[video['player']]['exp_bbbv_s'] = [video['bbbv_s'], video['file']]
             else:
                 ...
@@ -37,24 +37,24 @@ def stat(json_dir):
                 leaderboard[video['player']] = {}
                 leaderboard[video['player']]['beg_time'] = [video['rtime'], video['file']]
                 leaderboard[video['player']]['beg_bbbv_s'] = [video['bbbv_s'], video['file']]
-                leaderboard[video['player']]['int_time'] = 60
-                leaderboard[video['player']]['int_bbbv_s'] = 1
-                leaderboard[video['player']]['exp_time'] = 240
-                leaderboard[video['player']]['exp_bbbv_s'] = 0.5
+                leaderboard[video['player']]['int_time'] = [60, '']
+                leaderboard[video['player']]['int_bbbv_s'] = [1, '']
+                leaderboard[video['player']]['exp_time'] = [240, '']
+                leaderboard[video['player']]['exp_bbbv_s'] = [0.5, '']
             elif video['row'] == 16 and video['column'] == 16 and video['mine_num'] == 40:
                 leaderboard[video['player']] = {}
-                leaderboard[video['player']]['beg_time'] = 10
-                leaderboard[video['player']]['beg_bbbv_s'] = 2
+                leaderboard[video['player']]['beg_time'] = [10, '']
+                leaderboard[video['player']]['beg_bbbv_s'] = [2, '']
                 leaderboard[video['player']]['int_time'] = [video['rtime'], video['file']]
                 leaderboard[video['player']]['int_bbbv_s'] = [video['bbbv_s'], video['file']]
-                leaderboard[video['player']]['exp_time'] = 240
-                leaderboard[video['player']]['exp_bbbv_s'] = 0.5
+                leaderboard[video['player']]['exp_time'] = [240, '']
+                leaderboard[video['player']]['exp_bbbv_s'] = [0.5, '']
             elif video['row'] == 16 and video['column'] == 30 and video['mine_num'] == 99:
                 leaderboard[video['player']] = {}
-                leaderboard[video['player']]['beg_time'] = 10
-                leaderboard[video['player']]['beg_bbbv_s'] = 2
-                leaderboard[video['player']]['int_time'] = 60
-                leaderboard[video['player']]['int_bbbv_s'] = 1
+                leaderboard[video['player']]['beg_time'] = [10, '']
+                leaderboard[video['player']]['beg_bbbv_s'] = [2, '']
+                leaderboard[video['player']]['int_time'] = [60, '']
+                leaderboard[video['player']]['int_bbbv_s'] = [1, '']
                 leaderboard[video['player']]['exp_time'] = [video['rtime'], video['file']]
                 leaderboard[video['player']]['exp_bbbv_s'] = [video['bbbv_s'], video['file']]
             else:
